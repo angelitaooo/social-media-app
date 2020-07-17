748bc2b9f0a005b473b01fe0842099cb7c062f25
Angelita <angelita@Angelitas-MacBook-Pro.local>
2020-07-17 13:46:29 -0500

    create the side bar and style it

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,76 @@
-import React from 'react';
-import TweetList from './TweetList';
+import React from "react";
+import TweetList from "./TweetList";
 
 const App = () => {
-    return <div><TweetList /></div>
-}
+  return (
+    <div className="h-screen flex overflow-hidden bg-gray-100">
+      <div className="hidden md:flex md:flex-shrink-0">
+        <div className="flex flex-col w-64 bg-gray-800">
+          <div className="h-0 flex-1 flex flex-col overflow-y-auto">
+            <div className="flex items-center flex-shrink-0 px-4 pt-4">
+              <div>
+                <img
+                  className="inline-block h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+              </div>
+              <div className="ml-3">
+                <p className="text-base leading-6 font-medium text-white">
+                  Tom Cook
+                </p>
+                <p className="text-sm leading-5 font-medium text-gray-400 group-hover:text-gray-300 transition ease-in-out duration-150">
+                  View profile
+                </p>
+              </div>
+            </div>
+            <div>
+              <p className="pt-6 px-4 text-white">
+                this is the profile description his is the profile description
+              </p>
+            </div>
+
+            <nav className="mt-5 flex-1 px-2 bg-gray-800"></nav>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col w-0 flex-1 overflow-hidden">
+        <div className="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3">
+          <button
+            className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150"
+            aria-label="Open sidebar"
+          >
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+        <main
+          className="flex-1 relative z-0 overflow-y-auto focus:outline-none"
+          tabindex="0"
+        >
+          <div className="pt-2 pb-6 md:py-6">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <h1 className="text-2xl font-semibold text-gray-900">
+                Gossip Girl
+              </h1>
+            </div>
+            <TweetList></TweetList>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
 
 export default App;
